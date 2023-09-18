15e4c858d298368dc84a07e2f3f584640330048d
logicctdt <logicirz@gmail.com>
2023-09-18 16:42:52 -0400

    made tree initializer

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,17 @@
+import java.util.Optional;
+
 public class Tree {
+    private int root;
+    private Tree[] subtrees;
+    public Tree(int num, Tree[] subtreelist) {
+        root = num;
+        subtrees = subtreelist;
+
+    }
+
+    public Tree(int num) {
+        root = num;
+        subtrees = new Tree[] {};
+    }
+
 }
