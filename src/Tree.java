3e577056c4258fcf5ed401a9e6f22a11b14814b5
Chonarue <ismael.chonarueda@mail.utoronto.ca>
2023-09-18 16:57:57 -0400

    Added count, is_empty, classes

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,46 @@
+import java.util.Optional;
+
+import static java.util.Objects.isNull;
+
 public class Tree {
+    private Object root;
+    private Tree[] subtrees;
+    public Tree(int num, Tree[] subtreelist) {
+        root = num;
+        subtrees = subtreelist;
+
+    }
+
+    public Tree(int num) {
+        root = num;
+        subtrees = new Tree[] {};
+    }
+
+    public boolean is_empty()
+    {
+        if (isNull(this.root))
+        {
+            return True;
+        }
+        return False;
+
+    }
+
+    public int count()
+    {
+        if (this.is_empty())
+        {
+            return 0;
+        }
+        else {
+            int size = 1;
+            for (i =0; i<subtrees.length; i++)
+            {
+                size = size+subtrees[i].count();
+            }
+
+        }
+
+    }
+
 }
